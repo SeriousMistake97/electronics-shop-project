1d1bbe209c4007985da3cf2a212e34a4d3ab79a3
serious <sanekzh01@gmail.com>
2023-07-30 22:11:59 +0500

    add functions to the Item class

--- src/item.py
+++ src/item.py
@@ -1,3 +1,8 @@
+import csv
+
+from _pytest.nodes import Item
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +18,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
@@ -31,3 +36,31 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * Item.pay_rate
+
+    @property
+    def name(self) -> str:
+        """
+        Getter для получения имени товара.
+        """
+        return self.__name
+
+    @name.setter
+    def name(self, value: str) -> None:
+        """
+        Setter для имени товара.
+        """
+        if len(value) > 10:
+            self.__name = value[:10]
+        else:
+            self.__name = value
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('../src/items.csv', 'r', encoding='utf8') as csvfile:
+            csv_reader = csv.DictReader(csvfile)
+            for row in csv_reader:
+                Item(row['name'], cls.string_to_number(row['price']), int(row['quantity']))
+
+    @staticmethod
+    def string_to_number(string: str) -> float:
+        return int(float(string))
